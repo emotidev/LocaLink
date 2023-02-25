339375c137537db015db6a32a23c1c0a8a286918
Krish Gupta <krshg@outlook.com>
2023-02-25 07:22:58 +0000

    fix: I solved autehntication 🥺

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import 'kresco/styles/tailwind.css'
 
 import * as React from 'react'
@@ -5,6 +6,7 @@ import { NavigationMenu, BaseLayout } from 'kresco/esm/src'
 import { Poppins } from 'next/font/google'
 import Link from 'next/link'
 import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800', '900'],
@@ -34,7 +36,27 @@ export default async function RootLayout({
           </NavigationMenu.Item>
           {session ? (
             <NavigationMenu.Item>
-              <Link href="/api/auth/signout">Sign out</Link>
+              <NavigationMenu.Trigger>
+                <div className="flex items-center space-x-2">
+                  <img
+                    src={
+                      session.user?.image ??
+                      'https://dicebear.com/api/avataaars/seed.svg?m=6&b=%23f5f5f5&c=%23f5f5f5&h=32&w=32&s=32'
+                    }
+                    alt={session.user?.name ?? 'User'}
+                    width={32}
+                    height={32}
+                    className="rounded-full"
+                  />
+                </div>
+              </NavigationMenu.Trigger>
+              <NavigationMenu.Content direction="bottom-right">
+                <ul className="flex flex-col space-y-2">
+                  <NavigationMenu.Link href="/api/auth/signout">
+                    Sign out
+                  </NavigationMenu.Link>
+                </ul>
+              </NavigationMenu.Content>
             </NavigationMenu.Item>
           ) : (
             <NavigationMenu.Item>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,15 +2,28 @@ import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import prisma from "./prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import Auth0Provider from "next-auth/providers/auth0";
 
 const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER as string,
     })
   ],
   adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET as string,
+  session: {
+    generateSessionToken: () => {
+      return Math.random().toString(36).substr(2, 10)
+    },
+    strategy: "jwt"
+  },
 }
 
 export default authOptions
